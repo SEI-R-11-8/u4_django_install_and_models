662b83f797709f0b26f513a881d0aa46fa3c42d6
laurengrubb <lauren.elyse.grubb@gmail.com>
2022-08-31 12:16:51 -0400

    completed homeworK

--- settings.sql
+++ settings.sql
@@ -1,4 +1,4 @@
 -- settings.sql
 CREATE DATABASE Nostaldja;
-CREATE USER NostaldjaUser WITH PASSWORD 'homework';
-GRANT ALL PRIVILEGES ON DATABASE Nostaldja TO NostaldjaUser;
+CREATE USER nostaldjauser WITH PASSWORD 'homework';
+GRANT ALL PRIVILEGES ON DATABASE Nostaldja TO nostaldjauser;
